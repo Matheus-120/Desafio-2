61426d4e4a4574828be97a20cfcf8c9316025143
Matheus-120 <mathbaf@gmail.com>
2025-03-16 12:15:36 -0300

    Desafio-API-Rest-na-Nuvem

---
+++ src/test/java/Desafio_API_Rest_nuvem_SpringBoot3/Desafio_API_Rest_nuvem_SpringBoot3/DesafioApiRestNuvemSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio_API_Rest_nuvem_SpringBoot3.Desafio_API_Rest_nuvem_SpringBoot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiRestNuvemSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
